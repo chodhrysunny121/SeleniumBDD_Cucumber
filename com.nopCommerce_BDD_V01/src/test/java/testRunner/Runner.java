78b9eed3e19afcb5e4495912d7fe97985239571d
chodhrysunny121 <94341944+chodhrysunny121@users.noreply.github.com>
2023-02-23 10:28:45 -0500

    updated DryRun=false

--- com.nopCommerce_BDD_V01/src/test/java/testRunner/Runner.java
+++ com.nopCommerce_BDD_V01/src/test/java/testRunner/Runner.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 		features=".//Features/Login.feature",
 		glue="stepDefinitions",
-		dryRun=true,
+		dryRun=false,
 		monochrome=true,
 		plugin={"pretty","html:test-output"}
 		
